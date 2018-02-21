0fa1f2e4abe884cbe850522202b4ba5c27510fc9
vicencmm <vicenc.marquez@gmail.com>
2018-02-21 21:38:03 +0100

    Interactius

---
+++ interactius/weatherinf/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1519242783,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/weather-sheet0.png",
+		"images/weather-sheet1.png",
+		"images/weather-sheet2.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
